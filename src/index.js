e7086a851f49c3fe10efcb2717fc499d863e442e
Daria Poddubnaia <sablezzubaya@gmail.com>
2022-06-22 00:24:44 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = String(n);
+    let result = "";
+    for (let i = 0; i < n.length; i++) {
+        result = `${n[i]}${result}`;
+    }
+    result = parseInt(result);
+    return result;
+    //     let result = 0;
+    //     while (n) {
+    //         result = result * 10 + (n % 10);
+    //         n = Math.floor(n / 10);
+    //     }
+    //     return result;
+};
