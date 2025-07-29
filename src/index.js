be746eaa0cea14c91fec6fc0ee0a3bb8143974c3
Dariusssssssss <sablezzubaya@gmail.com>
2025-07-30 01:14:33 +0300

    add: upd solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const str = String(n);
+  let result = '';
+  for (let i = 0; i < str.length; i += 1) {
+    result = `${str[i]}${result}`;
+  }
+  result = parseInt(result, 10);
+  return result;
 };
